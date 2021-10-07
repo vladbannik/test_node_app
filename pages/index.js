471bde5d0fbdc8854845dfb4fd643bd1ebdac12e
vladyslav.bannyk <vladyslav.bannyk@digitalsuits.co>
2021-10-07 14:44:40 +0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>Hello, its my first app for Shopify by Vlad Bannik</Heading>
+  </Page>
+);
+
+export default Index;
